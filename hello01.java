25c4412e133f2c8f6075cfe556871b70ff032793
AllenLiu9324 <qqzhbc0823@gmail.com>
2021-01-10 13:08:28 +0800

    init commit

---
+++ hello01.java
@@ -0,0 +1,18 @@
+package Test01;
+
+
+
+/**
+ * @author 202011-CJ01008
+ *
+ */
+public class hello01 {
+	
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+		
+	}
+}
+
+
